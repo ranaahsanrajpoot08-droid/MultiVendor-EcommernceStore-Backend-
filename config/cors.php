04fdf5536cdb35bbe7e74d9467676f4e2b314060
Muhammad Ahsan Noor <ranaahsanrajpoot08@gmail.com>
2026-04-25 02:45:07 +0500

    Fix CORS for Vercel frontend

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'https://multi-vendor-ecommernce-store-front.vercel.app'
+        'https://multi-vendor-ecommernce-store-front-chi.vercel.app',
+        'https://multi-vendor-ecommernce-store-front.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
